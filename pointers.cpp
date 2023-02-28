4c6f34a1b255885ff368215d592f590376c32c27
Emirhan Bilgiç <82952986+emirhanbilgic@users.noreply.github.com>
2023-02-28 19:56:02 +0300

    Update pointers.cpp

--- pointers.cpp
+++ pointers.cpp
@@ -7,7 +7,7 @@ int main()
     int integer = 3;
     int *Pinteger = &integer;
     
-    
+    cout << &integer << endl; // 0x7fffeb2047fc
     cout << Pinteger << endl; // 0x7fffeb2047fc
     cout << *Pinteger << endl; // 3
     cout << &*Pinteger; // 0x7fffeb2047fc
